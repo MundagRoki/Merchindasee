03801d7832cda78ded5fd25831fbf821ddd43c81
Mundag Roki <140238284+MundagRoki@users.noreply.github.com>
2023-12-02 21:32:22 +0800

    update

---
+++ js/home.js
@@ -0,0 +1,7 @@
+// const mouseCont = document.querySelector(".mouse__container");
+// const mouse = document.querySelector(".mouse");
+// const container = document.querySelector("body");
+
+const scrollHandler = (event) => {
+  console.log(event);
+};
