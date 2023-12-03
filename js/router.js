f7ceb99e18f39cafaa256a65550883cc1fd30d56
era <86195138+erastyl@users.noreply.github.com>
2023-12-03 21:03:15 +0800

    plus products page

--- js/router.js
+++ js/router.js
@@ -1,23 +1,24 @@
 const route = (event) => {
-  event.preventDefault();
-  window.history.pushState({}, "", event.target.href);
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
 };
 
 const routes = {
-  404: "/pages/404.html",
-  "/": "../index.html",
-  "/about": "/pages/about.html",
-  "/home": "/pages/home.html",
+    404: "/pages/404.html",
+    "/": "../index.html",
+    "/products": "/pages/products.html",
+    "/about": "/pages/about.html",
+    "/home": "/pages/home.html",
 };
 
-const handleLocation = async () => {
-  const path = window.location.pathname;
-  let route;
-  if (path === "/") route = routes["/home"];
-  else route = routes[path] || routes[404];
+const handleLocation = async() => {
+    const path = window.location.pathname;
+    let route;
+    if (path === "/") route = routes["/home"];
+    else route = routes[path] || routes[404];
 
-  const html = await fetch(route).then((data) => data.text());
-  document.getElementById("root").innerHTML = html;
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("root").innerHTML = html;
 };
 
 window.onpopstate = handleLocation;
